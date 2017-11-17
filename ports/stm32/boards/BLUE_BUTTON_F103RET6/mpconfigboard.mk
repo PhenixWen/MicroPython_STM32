dbcdb85ef9a37d5f543b5f312d1d02b729b5bc84
PhenixWen <365401216@qq.com>
2017-11-17 11:23:35 +0800

    fix compile error

--- ports/stm32/boards/BLUE_BUTTON_F103RET6/mpconfigboard.mk
+++ ports/stm32/boards/BLUE_BUTTON_F103RET6/mpconfigboard.mk
@@ -2,3 +2,6 @@ MCU_SERIES = f1
 CMSIS_MCU = STM32F103xE
 AF_FILE = boards/stm32f103_af.csv
 LD_FILE = boards/stm32f103xe.ld
+
+# Don't include default frozen modules because MCU is tight on flash space
+FROZEN_MPY_DIR ?=
